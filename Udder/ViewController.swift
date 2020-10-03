b37a56be6691d0461f4c3d292f0303f49845a46d
England Yu <yyg0404@naver.com>
2020-10-03 19:43:50 +0900

    Add viewDidDisappear.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        <#code#>
+        //
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
     }
 }
 
